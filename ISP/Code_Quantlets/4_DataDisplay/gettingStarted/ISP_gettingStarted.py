f4f773141241b3f883dc94ee8ba2499d04db67ab
butayama <butayama@users.noreply.github.com>
2022-01-22 01:13:28 +0100

    manica studies Linefit VB_WF-PMD.ods / VB_WF-PMD.csv

--- ISP/Code_Quantlets/4_DataDisplay/gettingStarted/ISP_gettingStarted.py
+++ ISP/Code_Quantlets/4_DataDisplay/gettingStarted/ISP_gettingStarted.py
@@ -26,11 +26,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
+
 def main():
     """Define the main function. """
-    
+
     # Create a sine-wave
-    x = np.arange(0,10,0.1)
+    x = np.arange(0, 10, 0.1)
     y = np.sin(x)
 
     # Save the data in a text-file, in column form
@@ -38,12 +39,12 @@ def main():
     # get a matrix, you stack the two rows above each other, and then transpose
     # the matrix
     outFile = 'test.txt'
-    np.savetxt(outFile, np.column_stack((x,y)) )
+    np.savetxt(outFile, np.column_stack((x, y)))
 
     # Read the data into a different variable
     inData = np.loadtxt(outFile)
-    x_2 = inData[:,0] # Note that Python starts at "0"!
-    y_2 = inData[:,1]
+    x_2 = inData[:, 0]  # Note that Python starts at "0"!
+    y_2 = inData[:, 1]
 
     # Plot the data, and wait for the user to click
     plt.plot(x_2, y_2)
@@ -51,38 +52,39 @@ def main():
     plt.waitforbuttonpress()
 
     # Generate a noisy line
-    x = np.arange(-100,100)
+    x = np.arange(-100, 100)
     # use a Python "dictionary" to group the parameters
-    par = {'offset':100, 'slope':0.5, 'noiseAmp':4}
-    y = par['offset'] + par['slope']*x + par['noiseAmp']*np.random.randn(len(x))
+    par = {'offset': 100, 'slope': 0.5, 'noiseAmp': 4}
+    y = par['offset'] + par['slope'] * x + par['noiseAmp'] * np.random.randn(len(x))
 
     # Select "late" values, i.e. with x>10
     late = x > 10
     x_high = x[late]
     y_high = y[late]
-    
+
     # Determine the best-fit line
     res = stats.linregress(x_high, y_high)
 
     # Plot the "late" data, and the best-fit line
     plt.close()
-    plt.plot(x_high, y_high, label = 'raw data')
-    plt.plot(x_high, res.intercept + res.slope*x_high, 'r', label='fit')
-    
+    plt.plot(x_high, y_high, label='raw data')
+    plt.plot(x_high, res.intercept + res.slope * x_high, 'r', label='fit')
+
     out_file = 'line_fit.png'
     plt.savefig(out_file, dpi=200)
     plt.show()
-    
+
     print(f'Results saved to {out_file}')
-    
+
     # For the confidence interval of the slope we need the
     # two-sided inverse Students t-distribution
-    p = 0.05        # probability
-    df = len(x)-2   # degrees of freedom
-    
-    ts = np.abs(stats.t.ppf(p/2, df))
-    print(f"slope (95%): {res.slope:.3f} +/- {ts*res.stderr:.3f}")
+    p = 0.05  # probability
+    df = len(x) - 2  # degrees of freedom
+
+    ts = np.abs(stats.t.ppf(p / 2, df))
+    print(f"slope (95%): {res.slope:.3f} +/- {ts * res.stderr:.3f}")
     # >> slope (95%): 0.517 +/- 0.033 
 
-if __name__=='__main__':
-    main()    # Execute the main function
+
+if __name__ == '__main__':
+    main()  # Execute the main function
